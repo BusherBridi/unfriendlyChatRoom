4ce3a4a1262052f312b00d81041218f143984c88
Busher Bridi <bridibusher@gmail.com>
2020-07-04 16:59:52 -0700

    Added sqlalchemy libs and set up db

--- application.py
+++ application.py
@@ -2,10 +2,20 @@ import os
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from sqlalchemy import create_engine, exc
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-databaseUrl = os.getenv("DATABASE_URL")
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+# Set up socket
 socketio = SocketIO(app)
 
 
