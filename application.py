027434c46fbec37ef3c78dfd792cd5454eaa52f7
Busher Bridi <bridibusher@gmail.com>
2020-07-05 02:43:48 -0700

    Added login logic

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import scoped_session, sessionmaker
+import hashlib
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -27,3 +28,14 @@ def index():
 def message(data):
     msg = data["message"]
     emit("broadcast message", {"message":msg}, broadcast=True)
+
+@app.route("/login", methods = ["POST"])
+def login():
+    username = str(request.form.get("username").upper())
+    password = str(request.form.get("password"))
+    passwordHash = hashlib.sha256()
+    passwordHash.update(password.encode('utf8'))
+    hashedPassword = str(passwordHash.hexdigest())
+    if(db.execute("SELECT * FROM users WHERE upper(username) =:username AND password = :password", {"username": username, "password":hashedPassword}).rowcount == 1):
+        user = db.execute("SELECT username FROM users WHERE upper(username) =:username",{"username":username}).fetchone()
+        return render_template("dashboard.html", username=user.username)
