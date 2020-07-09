01b7ef15f5d31f54d40972473ceb891b03d12a0e
Busher Bridi <bridibusher@gmail.com>
2020-07-08 18:07:37 -0700

    Changed io logic

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,30 +1,55 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Connect to socket
+//     console.log("connecting");
+//     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+//     const username = document.querySelector("#username").innerHTML;
+//     socket.on('connect', () => {
+//         console.log("connected")
+//         document.querySelector("#sendMessage").onclick = () => {
+//             const msg = document.querySelector("#message").value;
+//             if (msg == "") {
+//                 alert("Cannot send empty message")
+//             } else {
+
+//                 socket.emit('post message', { 'message': msg, 'user': username });
+//                 console.log("sending message: " + msg)
+//             }
+//         };
+//     });
+
+//     //Recieve incoming messages:
+//     socket.on('broadcast message', data => {
+//         const li = document.createElement('li');
+//         li.innerHTML = `${data.user}: ${data.message}`;
+//         document.querySelector("#messages").append(li);
+//     })
+
+
+// });
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.cookie = "io=secure; test=1";
     // Connect to socket
     console.log("connecting");
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
+    // Get username
     const username = document.querySelector("#username").innerHTML;
-    socket.on('connect', () => {
-        console.log("connected")
-        document.querySelector("#sendMessage").onclick = () => {
-            const msg = document.querySelector("#message").value;
-            if (msg == "") {
-                alert("Cannot send empty message")
-            } else {
 
-                socket.emit('post message', { 'message': msg, 'user': username });
-                console.log("sending message: " + msg)
-            }
+    //Broadcast msg:
+
+    document.querySelector('#sendMessage').onclick = () => {
+        const msg = document.querySelector("#message").value;
+        if (msg == "") {
+            alert("Cannot send empty message")
+        } else {
+            socket.emit('post message', { 'message': msg, 'user': username })
+            console.log("sending message: " + msg)
         };
-    });
-
-    //Recieve incoming messages:
+    }
     socket.on('broadcast message', data => {
         const li = document.createElement('li');
         li.innerHTML = `${data.user}: ${data.message}`;
         document.querySelector("#messages").append(li);
     })
-
-
 });
