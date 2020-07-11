e842cf020db6a91cb9b37d897fde275fe70e143d
Paulo Alberto Sousa <p.sousa612@gmail.com>
2020-07-11 02:06:26 -0700

    UI Improvements

--- static/dashboard.js
+++ static/dashboard.js
@@ -7,23 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#themeSelect").onchange = () => {
         let body = document.querySelector("body");
 
+        //Resetting the body styles
         body.style.background = "";
         body.style.animation = "";
         body.style.backgroundSize = "";
 
-        var params = ["270deg, #e9ec5e, #ff2278", "270deg, #02bee8, #7a33bf", "270deg, #840029, #a91834, #f64d52"];
-        var rand = Math.floor(Math.random() * params.length);
-
+        //Set style based on selected theme
         switch (document.querySelector("#themeSelect").value) {
 
             case "basic":
                 body.style.color = "black";
                 body.style.backgroundColor = "white";
+                document.querySelector(".messageBar").backgroundColor = "white";
                 break;
 
             case "mustard":
                 body.style.color = "red";
                 body.style.backgroundColor = "yellow";
+                document.querySelector(".messageBar").backgroundColor = "white";
                 break;
 
             case "dark":
@@ -32,6 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 break;
 
             case "vibe":
+                var params = ["270deg, #e9ec5e, #ff2278", "270deg, #02bee8, #7a33bf", "270deg, #840029, #a91834, #f64d52"];
+                var rand = Math.floor(Math.random() * params.length);
+
                 body.style.color = "white";
                 body.style.background = "linear-gradient(" + params[rand] + ")";
                 body.style.animation = "grad 9s ease infinite";
@@ -55,19 +59,13 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             socket.emit('post message', { 'message': msg, 'user': username })
             console.log("sending message: " + msg);
-            document.querySelector("#message").value = "";
-
         };
     }
-    document.querySelector("#message").addEventListener("keyup", event => {
-        if (event.key !== "Enter") return;
-        document.querySelector("#sendMessage").click();
-        event.preventDefault();
-    });
     socket.on('broadcast message', data => {
         const li = document.createElement('li');
         console.log(`getting message: ${data.msg}`)
         li.innerHTML = `${data.user}: ${data.message}`;
         document.querySelector("#messages").append(li);
+        document.querySelector("#messages *:last-child").scrollIntoView();
     })
 });
