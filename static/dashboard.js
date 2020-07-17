dff4b820bad45c2f14b1d8cec98593c4f82f73e2
Busher Bridi <bridibusher@gmail.com>
2020-07-17 01:39:39 -0700

    Added client side code for saving dashboard changes

--- static/dashboard.js
+++ static/dashboard.js
@@ -7,6 +7,32 @@ document.addEventListener('DOMContentLoaded', () => {
     toggleHelp("bioText", "bioHelp");
     toggleHelp("urlText", "urlHelp");
     toggleHelp("locationText", "locationHelp");
+
+
+    //Trigger when user clicks save: 
+    document.querySelector("#saveBtn").onclick = () => {
+        const bio = document.querySelector("#bioText").value;
+        const announcement = document.querySelector("#announcement").value;
+        const url = document.querySelector("#urlText").value;
+        const location = document.querySelector("#locationText").value;
+        // Init new AJAX request:
+        const request = new XMLHttpRequest();
+        request.open('POST', '/savechanges');
+        request.onload = () => {
+
+            const data = JSON.parse(request.responseText);
+            console.log(data)
+        };
+        //Add data to request:
+        const data = new FormData();
+        data.append('bio', bio);
+        data.append('announcement', announcement);
+        data.append('url', url);
+        data.append('location', location);
+        //send req
+        request.send(data)
+        return false;
+    };
 });
 
 
@@ -16,7 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function toggleHelp(inputID, helpID) {
     inputID = "#" + inputID;
     helpID = "#" + helpID;
-    console.log(inputID);
     document.querySelector(inputID).onfocus = () => {
         document.querySelector(helpID).style.visibility = "visible";
     };
