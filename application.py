fdb574c9eba4a88789c4cc0c17d63785f47545c3
Busher Bridi <bridibusher@gmail.com>
2020-07-17 01:40:08 -0700

    Added server side code to update db with data send through AJAX

--- application.py
+++ application.py
@@ -21,6 +21,8 @@ db = scoped_session(sessionmaker(bind=engine))
 socketio = SocketIO(app)
 
 # Homepage/index route
+
+
 @app.route("/")
 def index():
     if(not session):
@@ -30,21 +32,26 @@ def index():
     else:
         return render_template("index.html")
 # login route
-@app.route("/login", methods = ["POST"])
+
+
+@app.route("/login", methods=["POST"])
 def login():
     username = str(request.form.get("username").upper())
     password = str(request.form.get("password"))
     passwordHash = hashlib.sha256()
     passwordHash.update(password.encode('utf8'))
     hashedPassword = str(passwordHash.hexdigest())
-    if(db.execute("SELECT * FROM users WHERE upper(username) =:username AND password = :password", {"username": username, "password":hashedPassword}).rowcount == 1):
-        user = db.execute("SELECT username FROM users WHERE upper(username) =:username",{"username":username}).fetchone()
+    if(db.execute("SELECT * FROM users WHERE upper(username) =:username AND password = :password", {"username": username, "password": hashedPassword}).rowcount == 1):
+        user = db.execute("SELECT username FROM users WHERE upper(username) =:username", {
+                          "username": username}).fetchone()
         session["logged_in"] = True
-        session["user_info"] = {"username":user.username}
+        session["user_info"] = {"username": user.username}
         return redirect(url_for('chatroom'))
     else:
         return("Wrong Username or Password")
 # Chat room route
+
+
 @app.route("/chatroom")
 def chatroom():
     if(not session):
@@ -54,6 +61,8 @@ def chatroom():
     else:
         return redirect(url_for('index'))
 # Sign up route
+
+
 @app.route("/signup")
 def signup():
     if(not session):
@@ -62,30 +71,23 @@ def signup():
         return "cannont create account while logged in"
     else:
         return render_template("signup.html")
-    
-# username check API
-@app.route("/checkUsername", methods = ["POST"])
-def validiation():
-    username = str(request.form.get("username"))
-    if(not username):
-        pass
-    else:
-        if(db.execute("SELECT username FROM users WHERE upper(username) =:username", {"username":username.upper()}).rowcount == 1):
-            return jsonify({"isTaken":True, "username":username})
-        else:
-            return jsonify({"isTaken":False, "username":username})
 
-# Dashboard Route 
+# Dashboard Route
+
+
 @app.route("/dashboard")
 def dashboard():
     if(not session):
-        return redirect(url_for('index')) #General error page instead of redirecting back to index?
+        # General error page instead of redirecting back to index?
+        return redirect(url_for('index'))
     elif(session["logged_in"] == True):
         return render_template("dashboard.html", username=session["user_info"]["username"])
     else:
         return redirect(url_for('index'))
-#Create user API
-@app.route("/createUser", methods = ["POST"])
+# Create user API
+
+
+@app.route("/createUser", methods=["POST"])
 def createUser():
     username = str(request.form.get("usernamesu"))
     password = str(request.form.get("passwordsu"))
@@ -98,21 +100,46 @@ def createUser():
     elif(password != passwordConf):
         return ("passwords didn't match")
     else:
-        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",{"username":username, "password":hashedPassword})
+        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {
+                   "username": username, "password": hashedPassword})
         db.commit()
         return ("User Created")
     return "error"
 
+# username check API
+
+
+@app.route("/checkUsername", methods=["POST"])
+def validiation():
+    username = str(request.form.get("username"))
+    if(not username):
+        pass
+    else:
+        if(db.execute("SELECT username FROM users WHERE upper(username) =:username", {"username": username.upper()}).rowcount == 1):
+            return jsonify({"isTaken": True, "username": username})
+        else:
+            return jsonify({"isTaken": False, "username": username})
+
 # Save dashboard changes API
-@app.route("/savechanges", methods = ["POST"])
+
+
+@app.route("/savechanges", methods=["POST"])
 def savechanges():
+    username = session["user_info"]["username"]
     bio = str(request.form.get("bio"))
-    announcement = str(request.form.get("announ"))
+    announcement = str(request.form.get("announcement"))
     url = str(request.form.get("url"))
     location = str(request.form.get("location"))
+    try:
+        db.execute("UPDATE users SET bio =:bio, announcement =:announcement, url =:url, location =:location WHERE username = :username", {"bio":bio, "announcement":announcement, "url":url,"location":location, "username":username})
+        db.commit()
+        data = jsonify({"saved":True})
+    except:
+        data = jsonify({"saved":False})
+    return data
+
     
-    
-#For use in chatroom
+# For use in chatroom
 @socketio.on("post message")
 def message(data):
     msg = data["message"]
